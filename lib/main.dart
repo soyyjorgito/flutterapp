09c2f7b2b3680934fa9a06217f6eb10f278319bd
Jorge Eslava <jorge08432@hotmail.com>
2023-03-05 16:58:54 -0500

    Assets

--- lib/main.dart
+++ lib/main.dart
@@ -8,66 +8,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contactos',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  List<String> names = [
-    "Jorge", 
-    "Kevin", 
-    "Tatiana",
-    "Mateo",
-    "Diego",
-    "Daniel",
-    "Daniela",
-    "Juliana",
-    "David",
-    "Santiago",
-    "Marcelo",
-    "Alejandro",
-    "Fernando",
-    "Roberto",
-    "Andres",
-    "Hugo",
-    "Pablo",
-    "Juan"
-    ];
-
-  MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Contactos'),
-        actions: const <Widget>[
-          IconButton(onPressed: _add, icon: Icon(Icons.add))
-        ],
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        backgroundColor: const Color.fromARGB(255, 15, 167, 1),
-      ),
-      body: ListView.builder(
-        itemCount: names.length,
-        itemBuilder: (BuildContext context, int index) {
-          final name = names[index];
-          return ListTile(
-            title: Text(name),
-            leading: const Icon(Icons.person),
-            onTap: ()
-            {
-              print(name);
-            }
-            );
-        },
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Assets'),
+        ),
+        body: ListView(
+          children: <Widget> [
+            Image.asset("assets/amongus.jpg"),
+            Image.asset("assets/amongus2.jpg"),
+          ],
+        ),
       ),
     );
   }
-}
-
-void _add()
-{
-
 }
