a74168569964e9abc070d58add39e472a0f415ce
Jorge Eslava <jorge08432@hotmail.com>
2023-03-05 12:39:39 -0500

    Uso de Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Uso del Column'),
+        ),
+        body: SizedBox(
+          width:double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                color: Colors.orange,
+                height:100,
+                width:100,
+                ),
+                Text("Hola, estoy en una columna"),
+                Image.network("https://media.tenor.com/4_E21LSI0ogAAAAi/banana-cheerer.gif")
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage();
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: Text("Uso del scaffold"), actions: <Widget>[
-        IconButton(onPressed: _add, icon: Icon(Icons.add)),
-        IconButton(onPressed: _remove, icon: Icon(Icons.remove))
-      ],
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-      ),
-      body: Center(),
-      floatingActionButton:
-          FloatingActionButton(child: Icon(Icons.add), onPressed: () => {}),
-      drawer: Drawer(
-        backgroundColor: Colors.pink,
-      ),
-      backgroundColor: Colors.black,
-    );
-  }
-
-  void _add() {
-    print("hola");
-  }
-
-  void _remove() {
-    print("adios");
   }
 }
